0354653fb6f170197d8ac9b11c8dff91a8adc09d
Tanish <146003771+SocialisticO4@users.noreply.github.com>
2025-11-22 10:15:57 +0530

    Remove deprecated statusBarColor usage

--- app/src/main/java/com/example/hida/ui/theme/Theme.kt
+++ app/src/main/java/com/example/hida/ui/theme/Theme.kt
@@ -60,7 +60,6 @@ fun HidaTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.background.toArgb()
             WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
